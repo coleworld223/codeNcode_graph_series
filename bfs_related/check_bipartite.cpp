636d31bc65ce770827817a9872aafdf12e82b650
coleworld223 <106908583+coleworld223@users.noreply.github.com>
2023-09-18 23:01:48 +0530

    Create check_bipartite.cpp

---
+++ bfs_related/check_bipartite.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main () {
+    // int tests; cin >> tests;
+    int tests = 1;
+
+    auto solve = [&] () {
+        ll n, m; cin >> n >> m;
+
+        vector <vector <ll>> adj (n);
+
+        // sssp algo for trees using dfs 
+
+        for (ll i = 0; i < m; i++) {
+            ll u, v; cin >> u >> v; u--; v--;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        bool bi = 1;
+
+        vector <ll> col (n, -1);
+
+        function <void (ll, ll)> dfs = [&] (ll node, ll c) -> void {
+            col[node] = c;
+            for (auto child : adj[node]) {
+                if (col[child] == -1) {
+                    dfs (child, 1 - c);
+                }
+                else if (col[child] == c) {
+                    bi = 0;
+                }
+            }
+        };
+
+        function <void (ll node)> bfs = [&] (ll node) -> void {
+            queue <ll> q;
+            q.push(node);
+            col[node] = 0;
+
+            while (!q.empty()) {
+                ll c_node = q.front(); q.pop();
+                for (auto child : adj[node]) {
+                    if (col[child] == -1) {
+                        col[child] = 1 - col[c_node];
+                        q.push(child);
+                    }
+                    else if (col[child] == col[c_node]) {
+                        bi = 0;
+                    }
+                }
+            }
+        };
+
+        for (ll i = 0; i < n; i++) {
+            if (col[i] == -1) {
+                // dfs (i, 0);
+                bfs (i);
+            }
+        }
+
+        if (bi) {
+            for (ll i = 0; i < n; i++) {
+                cout << col[i] << " ";
+            }
+        }
+        else {
+            cout << "Not bipartite" << endl;
+        }
+
+    };
+
+    for (int i = 0; i < tests; i += 1) {
+        solve ();
+    }
+}
